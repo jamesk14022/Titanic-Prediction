8e354a792ac3d80618ad687f63b07499983ff218
James Kingsbury <jameskingsbury919@gmail.com>
2019-10-07 02:14:13 +0100

    added sktree

--- sktree.py
+++ sktree.py
@@ -3,9 +3,11 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import GridSearchCV, train_test_split,cross_val_score, cross_validate, ShuffleSplit
 import matplotlib.pyplot as plt
 
 
@@ -109,49 +111,76 @@ x_test_kaggle = x_test
 
 print(x_train)
 
+params = {
+    
+}
+
 y_train = x_train.pop('Survived')
 
 # splitting complete kaggle training data further to implement early stopping 
 x_train_final, x_test_final, y_train_final, y_test_final = train_test_split(x_train, y_train, test_size = 0.35)
 
+tree = DecisionTreeClassifier(max_depth = 4, random_state = 0)
+base = cross_val_score(tree, x_train_final, y_train_final)
+fit = tree.fit(x_train_final, y_train_final)
 
-params = {
-    'max_depth': [6],
-    'min_child_weight': [0.8, 0.9],
-    'learning_rate': [0.01],
-    'n-estimators': [300]
-}
+preds = tree.predict(x_test_final)
 
-clf = xgb.XGBClassifier(base_score=0.35, booster='gbtree',
-       nthread=None, objective='binary:logistic')
+acc = accuracy_score(y_test_final, preds)
+print("Accuracy on pre RFE testing data: %.2f%%" % (acc * 100.0))
 
-cv = GridSearchCV(clf, params, scoring = 'accuracy', cv=4)
+tree_rfe = RFECV(tree, step = 1, scoring = 'accuracy')
+fit = tree_rfe.fit(x_train_final, y_train_final)
 
-fit = cv.fit(x_train, y_train)
-y_pred_train = cv.best_estimator_.predict(x_train)
-y_pred_test = cv.best_estimator_.predict(x_test_final)
+#PLot # of features selected vs. Model Score
+plt.figure()
+plt.title('XGB CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Neg Mean Squared Error")
+plt.plot(range(1, len(fit.grid_scores_) + 1), fit.grid_scores_)
+plt.show()
 
-y_pred_test_kaggle = cv.best_estimator_.predict(x_test_kaggle)
+#get rank of X model features
+print(fit.ranking_)
 
-print("The best parameters are %s with a score of %0.2f"
-      % (cv.best_params_, cv.best_score_))
 
-y_pred_test_ids = pd.DataFrame({ 'PassengerId': test_ids, 'Survived': y_pred_test_kaggle})
+x_train_rfe = x_train_final[x_train_final.columns.values[tree_rfe.get_support()]]
+x_test_rfe = x_test_final[x_test_final.columns.values[tree_rfe.get_support()]]
+x_test_kaggle = x_test_kaggle[x_test_kaggle.columns.values[tree_rfe.get_support()]]
+
+redClass = DecisionTreeClassifier(random_state = 0)
+redfit = redClass.fit(x_train_rfe, y_train_final)
+
+redpreds = redClass.predict(x_test_rfe)
+
+acc = accuracy_score(y_test_final, redpreds)
+print("Accuracy on post RFE pre tune testing data: %.2f%%" % (acc * 100.0))
+
+param_grid = {'criterion': ['gini', 'entropy'],  #scoring methodology; two supported formulas for calculating information gain - default is gini
+              #'splitter': ['best', 'random'], #splitting methodology; two supported strategies - default is best
+              'max_depth': [2,3,4,6], #max depth tree can grow; default is none
+              'min_samples_split': [.03], #minimum subset size BEFORE new split (fraction is % of total); default is 2
+              #'min_samples_leaf': [1,5,10,.03,.05], #minimum subset size AFTER new split split (fraction is % of total); default is 1
+              #'max_features': [None, 'auto'], #max features to consider when performing split; default none or all
+              'random_state': [0] #seed or control random number generator: https://www.quora.com/What-is-seed-in-random-number-generation
+             }
+
+tree_tune_rfe = GridSearchCV(DecisionTreeClassifier(), param_grid=param_grid, scoring = 'roc_auc', cv = 4,  return_train_score=True)
+tree_tune_rfe.fit(x_train_rfe, y_train_final)
+
+print(tree_tune_rfe.cv_results_.keys())
+print(tree_tune_rfe.best_params_)
+
+print(tree_tune_rfe.cv_results_['mean_train_score'])
+print(tree_tune_rfe.cv_results_['mean_test_score'])
+
+
+tunepreds = tree_tune_rfe.best_estimator_.predict(x_test_rfe)
+acc = accuracy_score(y_test_final, tunepreds)
+print("Accuracy on tuned test data: %.2f%%" % (acc * 100.0))
+
+
+kagglepreds = tree_tune_rfe.best_estimator_.predict(x_test_kaggle)
+y_pred_test_ids = pd.DataFrame({ 'PassengerId': test_ids, 'Survived': kagglepreds})
 y_pred_test_ids.Survived = y_pred_test_ids.Survived.round().astype("int")
-y_pred_test_ids.to_csv('submission.csv', sep=',', encoding='utf-8', index=False)
-
-
-acc = accuracy_score(y_train, y_pred_train)
-print("Accuracy on training data: %.2f%%" % (acc * 100.0))
-
-acc = accuracy_score(y_test_final, y_pred_test)
-print("Accuracy on testing data: %.2f%%" % (acc * 100.0))
-
-# feature_importances = pd.Series(cv.best_estimator_.feature_importances_, index = x_train.columns)
-# feature_importances.nlargest(40).plot(kind='barh')
-
-# plt.show(block=True)
-
-
-
-
+y_pred_test_ids.to_csv('submissionskl.csv', sep=',', encoding='utf-8', index=False)
--- titrfe.py
+++ titrfe.py
@@ -6,6 +6,7 @@ import xgboost as xgb
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_selection import RFECV
 import matplotlib.pyplot as plt
 
 
@@ -115,43 +116,23 @@ y_train = x_train.pop('Survived')
 x_train_final, x_test_final, y_train_final, y_test_final = train_test_split(x_train, y_train, test_size = 0.35)
 
 
-params = {
-    'max_depth': [6],
-    'min_child_weight': [0.8, 0.9],
-    'learning_rate': [0.01],
-    'n-estimators': [300]
-}
-
 clf = xgb.XGBClassifier(base_score=0.35, booster='gbtree',
        nthread=None, objective='binary:logistic')
 
-cv = GridSearchCV(clf, params, scoring = 'accuracy', cv=4)
+clfrfe = RFECV(clf, step=1, min_features_to_select=1, cv=4, scoring='neg_mean_absolute_error')
 
-fit = cv.fit(x_train, y_train)
-y_pred_train = cv.best_estimator_.predict(x_train)
-y_pred_test = cv.best_estimator_.predict(x_test_final)
+fit = clfrfe.fit(x_train, y_train)
 
-y_pred_test_kaggle = cv.best_estimator_.predict(x_test_kaggle)
+#PLot # of features selected vs. Model Score
+plt.figure()
+plt.title('XGB CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Neg Mean Squared Error")
+plt.plot(range(1, len(fit.grid_scores_) + 1), fit.grid_scores_)
+plt.show()
 
-print("The best parameters are %s with a score of %0.2f"
-      % (cv.best_params_, cv.best_score_))
+print(x_train.columns)
 
-y_pred_test_ids = pd.DataFrame({ 'PassengerId': test_ids, 'Survived': y_pred_test_kaggle})
-y_pred_test_ids.Survived = y_pred_test_ids.Survived.round().astype("int")
-y_pred_test_ids.to_csv('submission.csv', sep=',', encoding='utf-8', index=False)
-
-
-acc = accuracy_score(y_train, y_pred_train)
-print("Accuracy on training data: %.2f%%" % (acc * 100.0))
-
-acc = accuracy_score(y_test_final, y_pred_test)
-print("Accuracy on testing data: %.2f%%" % (acc * 100.0))
-
-# feature_importances = pd.Series(cv.best_estimator_.feature_importances_, index = x_train.columns)
-# feature_importances.nlargest(40).plot(kind='barh')
-
-# plt.show(block=True)
-
-
-
+#get rank of X model features
+print(fit.ranking_)
 
